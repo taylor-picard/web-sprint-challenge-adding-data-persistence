c355c91b0ffa244b84e8be7eaf52e447e2eecef3
Taylor Picard <taylorkpicard1998@gmail.com>
2023-09-29 12:53:24 -0500

    Changed port & connected projectRouter

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
-// build your `/api/projects` router here
+const router = require('express').Router();
+
+
+router.use('*', (req, res)=> {
+    res.status(200).json({
+        message: 'API reached!'
+    })
+})
+router.use((err, req, res)=> {
+    res.status(500).json({
+        message: err.message
+    })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const projectRouter = require('./project/router');
 
 const server = express();
-
 server.use(express.json());
+
+server.use('/api/projects', projectRouter);
 
 
 module.exports = server;

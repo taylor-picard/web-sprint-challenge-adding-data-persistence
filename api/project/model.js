84b4e0536a8f4e8d82f3b37fad0a41d20b0f92da
Taylor Picard <taylorkpicard1998@gmail.com>
2023-10-02 19:06:21 -0500

    projects model & router working

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,16 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+    return db('projects')
+}
+function postProject(newProject){
+    return db('projects').insert(newProject)
+        .then(([project_id]) => {
+            return db('projects').where('project_id', project_id)
+        })
+}
+
+ module.exports = {
+    getProjects,
+    postProject
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,35 @@
 const router = require('express').Router();
+const Project = require('./model');
 
+router.get('/', (req, res, next)=> {
+    Project.getProjects()
+        .then(projs => {
+            projs.forEach(project => {
+                if(project.project_completed === 1){
+                    project.project_completed = true;
+                }else{
+                    project.project_completed = false;
+                }
+            })
+            res.status(200).json(projs)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next)=> {
+    Project.postProject(req.body)
+        .then(newProject => {
+            newProject.forEach(project => {
+                if(project.project_completed === 1){
+                    project.project_completed = true;
+                }else{
+                    project.project_completed = false;
+                }
+            })
+            res.status(201).json(newProject[0])
+        })
+        .catch(next);
+})
 
 router.use('*', (req, res)=> {
     res.status(200).json({

de6dc2475bad9238bcfa1d1f8ebdd8a541b977ba
Taylor Picard <taylorkpicard1998@gmail.com>
2023-10-04 17:23:20 -0500

    Task model & router working - all tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,20 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+function getTasks() {
+    return db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select('t.*','p.project_name', 'p.project_description')
+}
+
+
+function postTask(newTask) {
+    return db('tasks').insert(newTask)
+        .then(([task_id])=> {
+            return db('tasks').where('task_id', task_id)
+        })
+}
+
+module.exports = {
+    getTasks,
+    postTask
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
+const Task = require('./model');
 
+router.get('/', (req, res, next)=> {
+    Task.getTasks()
+        .then(tasks => {
+            tasks.forEach(task => {
+                if(task.task_completed === 1){
+                    task.task_completed = true;
+                }else{
+                    task.task_completed = false;
+                }
+            })
+            res.status(200).json(tasks)
+        })
+        .catch(next)
+            
+})
+
+router.post('/', (req, res, next)=> {
+    Task.postTask(req.body)
+    .then(newTask => {
+        newTask.forEach(task => {
+            if(task.task_completed === 1){
+                task.task_completed = true;
+            }else{
+                task.task_completed = false;
+            }
+        })
+        res.status(201).json(newTask[0])
+    })
+    .catch(next)
+})
 
 router.use('*', (req, res)=> {
     res.status(200).json({
